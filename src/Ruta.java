05af2b4928d84d8ab8551123092b833595870ca8
Estarlin Valerio <valerioestarlin4tog@gmail.com>
2025-11-03 11:57:23 -0400

    Creacion de clases Parada y Ruta con atributos

--- src/Ruta.java
+++ src/Ruta.java
@@ -1,9 +1,68 @@
+import java.util.UUID;
+
 public class Ruta {
     //Arista
-    int tiempo; //En minutos
-    int distancia; //En kilomentros
-    int costo; //En pesos
-    String medio;
+    private UUID id;
+    private UUID id_origen;
+    private UUID id_destino;
+    private int tiempo; //En minutos
+    private int costo; //En pesos
+    private String medio;
 
+    public Ruta(UUID id_origen, UUID id_destino, int tiempo, int costo, String medio) {
+        this.id = UUID.randomUUID();
+        this.id_origen = id_origen;
+        this.id_destino = id_destino;
+        this.tiempo = tiempo;
+        this.costo = costo;
+        this.medio = medio;
+    }
 
+    public String getMedio() {
+        return medio;
+    }
+
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public UUID getId_destino() {
+        return id_destino;
+    }
+
+    public void setId_destino(UUID id_destino) {
+        this.id_destino = id_destino;
+    }
+
+    public UUID getId_origen() {
+        return id_origen;
+    }
+
+    public void setId_origen(UUID id_origen) {
+        this.id_origen = id_origen;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
